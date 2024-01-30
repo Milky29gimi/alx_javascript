06937742997b61bd14fa15e21fd74e4ee9b4378a
Milky Kerenso <milkykerenso@gmail.com>
2024-01-30 18:52:39 +0300

    commit 5

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,15 +1,8 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i].replace('12', '89'));
-}
+#!/usr/bin/node
 const myObject = {
+    type: 'object',
     value: 12
   };
-  
   console.log(myObject);
-  const myObject1 = { type: 'object', value: 12 };
-const myObject2 = { type: 'object', value: 89 };
-
-console.log(myObject1);
-console.log(myObject2);
+  myObject.value = 89
+  console.log(myObject);
--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,9 @@
-function add(a, b) {
-    return a + b;
-}
+#!/usr/bin/node
+//a function that returns the addition of 2 integers
+
+exports.add = (a, b) => a + b
+
+//can also be written as
+// function add(a, b) {
+//     return a + b;
+//   }
