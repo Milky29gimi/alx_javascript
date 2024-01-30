91c98b3a658da34b269cbd2d99f92fc49f30b6ab
Milky Kerenso <milkykerenso@gmail.com>
2024-01-30 18:36:00 +0300

    commit 5

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -3,3 +3,8 @@ const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0; i < lines.length; i++) {
     console.log(lines[i].replace('12', '89'));
 }
+const myObject = {
+    value: 12
+  };
+  
+  console.log(myObject);
